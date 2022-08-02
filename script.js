2e79e4461a8d115d402bfbe0a4de57a4bab1fdbe
paololabriola <paololabriola02@gmail.com>
2022-08-02 16:45:17 +0200

    Exercise completed

---
+++ script.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('data-insert');const scontoMinorenni = 0.2;
+
+const scontoAnziani = 0.4;
+const etaLimiteGiovani = 18;
+const etaLimiteAnziani = 65;
+const prezzoPerKm = 0.21;
+let prezzoBase = 0;
+
+function calcolaPrezzo(km, eta) {
+
+    prezzoBase = km * prezzoPerKm;
+
+    if(eta < etaLimiteGiovani)
+        return prezzoBase - (prezzoBase * scontoMinorenni) + "€";        
+    else if(eta > etaLimiteGiovani)
+        return prezzoBase - (prezzoBase * scontoAnziani) + "€";
+    else 
+        return prezzoBase + "€";
+
+}
+
+form.addEventListener('submit', addItem);
+
+function addItem(event) {
+
+    event.preventDefault();
+
+    const km = document.getElementById('km-input').value;
+    const eta = document.getElementById('eta-input').value;
+    const prezzoBiglietto = calcolaPrezzo(km, eta);
+    const div = document.getElementById("stampa-prezzo");
+    const h3 = document.createElement('h3');
+
+    h3.innerHTML = "Distanza: " + km + " Età: " + eta + " Prezzo: " + prezzoBiglietto;
+    div.appendChild(h3);
+
+}
